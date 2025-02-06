695ddbb114fa918a2e1e8f2f74e08a3365463d31
davtyan017 <davtyann.hayk@gmail.com>
2025-02-06 19:52:09 +0400

    href

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = ({ searchQuery, setSearchQuery, handleSearch }) => {
   return (
     <nav className="nav">
       <div>
-        <a href="#">
+        <a href="https://filmsdavtyan.netlify.app/">
           <img className="netflix" src={netflix} alt="" />
         </a>
-        <a href="#">browse</a>
+        <a href="https://filmsdavtyan.netlify.app/">browse</a>
         <p>DVD</p>
       </div>
       <div>
